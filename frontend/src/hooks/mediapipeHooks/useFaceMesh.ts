ebffe0845d85611c9208d2d5c0d928507156e3ef
이승민 <lsm5631@naver.com>
2024-11-18 15:24:11 +0900

    [#S11P31S209-545] [INFRA] fix: 환경설정 변경

--- frontend/src/hooks/mediapipeHooks/useFaceMesh.ts
+++ frontend/src/hooks/mediapipeHooks/useFaceMesh.ts
@@ -18,12 +18,10 @@ const useFaceMesh = (
     endTime: Date,
     type: string
   ) => {
-    // 시작 시간과 종료 시간을 recordStartTime으로부터의 경과 시간으로 계산
     const detectedMilliseconds =
       detectedTime.getTime() - recordStartTime.getTime();
     const endMilliseconds = endTime.getTime() - recordStartTime.getTime();
 
-    // 경과 시간을 시, 분, 초로 변환
     const formatElapsedTime = (milliseconds: number) => {
       const seconds = Math.floor(milliseconds / 1000) % 60;
       const minutes = Math.floor(milliseconds / (1000 * 60)) % 60;
@@ -54,9 +52,9 @@ const useFaceMesh = (
   useEffect(() => {
     if (!videoRef.current) return;
 
+    // FaceMesh 인스턴스를 생성할 때 로컬 파일을 사용하도록 설정
     const faceMesh = new FaceMesh({
-      locateFile: (file) =>
-        `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`,
+      locateFile: (file) => `${window.location.origin}/mediapipe/${file}`,
     });
 
     faceMesh.setOptions({
@@ -75,49 +73,38 @@ const useFaceMesh = (
 
       if (!ctx || !video) return;
 
-      // 캔버스 초기화 및 비디오 프레임 그리기 (얼굴이 없더라도 계속 그리기)
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-      // 얼굴 인식 결과가 없을 때
       if (
         !results.multiFaceLandmarks ||
         results.multiFaceLandmarks.length === 0
       ) {
         if (noFaceStartTimeRef.current === null) {
-          // 얼굴이 처음 사라진 시간 기록
           noFaceStartTimeRef.current = new Date();
           setNoFaceStartTime(noFaceStartTimeRef.current);
         }
         return;
       }
 
-      // 얼굴이 감지되었을 때
       if (noFaceStartTimeRef.current !== null) {
-        // 얼굴이 다시 나타났을 때 종료 시간 기록
         const currentEndTime = new Date();
         setNoFaceEndTime(currentEndTime);
 
-        // 이상행동 서버로 전송 (부재 상태)
         fetchPostAbnormal(
           noFaceStartTimeRef.current,
           currentEndTime,
           "absence"
         );
 
-        // 얼굴이 감지되었으므로 시작 시간 초기화
         noFaceStartTimeRef.current = null;
       }
 
-      // 2명 이상의 얼굴이 감지되었을 때 overcrowded로 전송
       if (results.multiFaceLandmarks.length > 1) {
         const currentTime = new Date();
-
-        // overcrowded 상태로 이상행동 서버로 전송
         fetchPostAbnormal(currentTime, currentTime, "overcrowded");
       }
 
-      // 얼굴이 인식된 경우 onResults 콜백 함수 호출
       onResults(results);
     });
 
@@ -132,6 +119,10 @@ const useFaceMesh = (
     });
 
     camera.start();
+
+    return () => {
+      camera.stop();
+    };
   }, [videoRef, onResults]);
 
   return { noFaceStartTime, noFaceEndTime };
